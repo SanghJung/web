193c1114c9902dcbcd01560b1bd75d75ac3af5c0
SanghJung <102583027+SanghJung@users.noreply.github.com>
2022-11-28 03:06:47 +0900

    Delete 웹프로그래밍 - 팀 프로젝트 directory

--- 웹프로그래밍 - 팀 프로젝트/js/arrowScroll.js
+++
@@ -1,6 +0,0 @@
-const arrowBtn = document.querySelector('.content__arrow');
-const aboutSection2 = document.querySelector('.about__section').offsetTop;
-
-arrowBtn.addEventListener('click', () => {
-    window.scroll({top: aboutSection2 - 78, behavior: "smooth"});
-})
